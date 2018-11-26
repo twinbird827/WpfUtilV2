4db9e1c117b338121906f340f22729f790acad16
twinbird827 <twinbird827@gmail.com>
2018-11-26 17:54:50 +0900

    TODO 20181126 C

---
+++ Mvvm/CustomControls/ChartEx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfUtilV2.Mvvm.CustomControls
+{
+    public class ChartEx : Image
+    {
+
+    }
+}
