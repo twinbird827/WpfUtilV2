36cdf5f86b2926638039c639b048f12112e83ca7
twinbird827 <35794567+twinbird827@users.noreply.github.com>
2018-11-29 01:40:55 +0900

    TODO 20181128 J

--- Mvvm/CustomControls/ListBoxEx.cs
+++ Mvvm/CustomControls/ListBoxEx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,5 +27,25 @@ namespace WpfUtilV2.Mvvm.CustomControls
                 item.IsSelected = true;
         }
 
+        /// <summary>
+        /// ItemsSource項目数変更時ｲﾍﾞﾝﾄ
+        /// </summary>
+        private void lvw_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (Items.Count == 0) return;
+
+            ScrollIntoView(Items[0]);
+        }
+
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+        {
+            if (!object.Equals(oldValue, newValue) && newValue != null)
+            {
+                (newValue as INotifyCollectionChanged).CollectionChanged += 
+                    new NotifyCollectionChangedEventHandler(lvw_CollectionChanged);
+            }
+
+            base.OnItemsSourceChanged(oldValue, newValue);
+        }
     }
 }
