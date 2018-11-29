947a4f9f8ad1c652d683f61aad110db85c4a7acf
twinbird827 <twinbird827@gmail.com>
2018-11-29 18:03:22 +0900

    TODO 20181129 C

--- Common/WpfUtil.cs
+++ Common/WpfUtil.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Runtime.InteropServices;
 
 namespace WpfUtilV2.Common
 {
@@ -18,5 +20,50 @@ namespace WpfUtilV2.Common
                 .GetMetadata(typeof(DependencyObject))
                 .DefaultValue;
         }
+
+        /// <summary>
+        /// システムから画面の DPI 設定を取得します。
+        /// </summary>
+        /// <param name="orientation">画面の方向</param>
+        /// <returns></returns>
+        public static Int32 GetDpi(Orientation orientation)
+        {
+            Int32 capIndex = (orientation == Orientation.Horizontal) ? 0x58 : 90;
+            using (DCSafeHandle handle = UnsafeNativeMethods.CreateDC("DISPLAY"))
+            {
+                return (handle.IsInvalid ? 0x60 : UnsafeNativeMethods.GetDeviceCaps(handle, capIndex));
+            }
+        }
+
     }
+
+    internal sealed class DCSafeHandle : Microsoft.Win32.SafeHandles.SafeHandleZeroOrMinusOneIsInvalid
+    {
+        private DCSafeHandle() : base(true) { }
+
+        protected override Boolean ReleaseHandle()
+        {
+            return UnsafeNativeMethods.DeleteDC(base.handle);
+        }
+    }
+
+    [System.Security.SuppressUnmanagedCodeSecurity]
+    internal static class UnsafeNativeMethods
+    {
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern Boolean DeleteDC(IntPtr hDC);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern Int32 GetDeviceCaps(DCSafeHandle hDC, Int32 nIndex);
+
+        [DllImport("gdi32.dll", EntryPoint = "CreateDC", CharSet = CharSet.Auto)]
+        public static extern DCSafeHandle IntCreateDC(String lpszDriver,
+            String lpszDeviceName, String lpszOutput, IntPtr devMode);
+
+        public static DCSafeHandle CreateDC(String lpszDriver)
+        {
+            return UnsafeNativeMethods.IntCreateDC(lpszDriver, null, null, IntPtr.Zero);
+        }
+    }
+
 }
