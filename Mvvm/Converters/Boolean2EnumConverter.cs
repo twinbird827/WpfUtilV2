c8be7fce1eb52ecb2226ea4cfac89a2dde397de2
twinbird827 <twinbird827@gmail.com>
2018-12-10 17:53:43 +0900

    [TODO] 20181210 C

---
+++ Mvvm/Converters/Boolean2EnumConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfUtilV2.Mvvm.Converters
+{
+    public class Boolean2EnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var ParameterString = parameter as string;
+            if (ParameterString == null)
+            {
+                return System.Windows.DependencyProperty.UnsetValue;
+            }
+
+            if (Enum.IsDefined(value.GetType(), value) == false)
+            {
+                return System.Windows.DependencyProperty.UnsetValue;
+            }
+
+            object paramvalue = Enum.Parse(value.GetType(), ParameterString);
+
+            return (int)paramvalue == (int)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var ParameterString = parameter as string;
+            return ParameterString == null
+                ? System.Windows.DependencyProperty.UnsetValue 
+                : Enum.Parse(targetType, ParameterString);
+        }
+    }
+}
