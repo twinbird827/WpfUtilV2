741fe81a04c5ed9298580af44de31f676dcb0dc5
twinbird827 <twinbird827@gmail.com>
2019-02-05 09:50:35 +0900

    a

--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -22,5 +22,15 @@ namespace WpfUtilV2.Extensions
             await ((Task)task).Timeout(timeout);
             return await task;
         }
+
+        public static Task WhenAll(this IEnumerable<Task> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
+        public static Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
     }
 }
