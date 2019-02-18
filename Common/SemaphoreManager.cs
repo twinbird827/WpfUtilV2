13fde84b83f5d688ee07a01d52d29c65bd065a7b
twinbird827 <twinbird827@gmail.com>
2019-02-18 17:53:25 +0900

    ﾛｯｸ処理の共通化

---
+++ Common/SemaphoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfUtilV2.Common
+{
+    public static class SemaphoreManager
+    {
+        private const string DefaultKey = "default";
+
+        private static Dictionary<string, SemaphoreSlim> Semaphores { get; set; } = new Dictionary<string, SemaphoreSlim>();
+
+        public static async Task WaitAsync()
+        {
+            await WaitAsync(DefaultKey);
+        }
+
+        public static async Task WaitAsync(string key)
+        {
+            if (!Semaphores.ContainsKey(key))
+            {
+                Semaphores.Add(key, new SemaphoreSlim(1, 1));
+            }
+            await Semaphores[key].WaitAsync();
+        }
+
+        public static int Release()
+        {
+            return Release(DefaultKey);
+        }
+
+        public static int Release(string key)
+        {
+            return Semaphores[key].Release();
+        }
+    }
+}
