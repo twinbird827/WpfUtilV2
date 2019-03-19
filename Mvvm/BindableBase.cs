f76e303271314f80f5168c4c2ee7d36502f2e101
twinbird827 <twinbird827@gmail.com>
2019-03-19 09:22:39 +0900

    using追加

--- Mvvm/BindableBase.cs
+++ Mvvm/BindableBase.cs
@@ -7,6 +7,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Threading;
 
 namespace WpfUtilV2.Mvvm
