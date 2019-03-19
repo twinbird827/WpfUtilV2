1122b672a0d7f4511aa110b6999325ac0c3eeea8
twinbird827 <twinbird827@gmail.com>
2019-03-19 09:19:22 +0900

    列挙型の表示文字取得用拡張ﾒｿｯﾄﾞ

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace WpfUtilV2.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetLabel<T>(this T target) where T : Enum
+        {
+            var targetString = target.ToString();
+            return typeof(T).GetField(targetString).GetCustomAttribute<LabelAttribute>()?.Value
+                ?? targetString;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class LabelAttribute : Attribute
+    {
+        public LabelAttribute(string label)
+        {
+            this.Value = label;
+        }
+
+        public string Value
+        {
+            get;
+        }
+    }
+}
