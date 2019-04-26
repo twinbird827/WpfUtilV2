aa50e92b1e04e5a228af1cf253981d04929edbef
twinbird827 <twinbird827@gmail.com>
2019-04-26 13:11:47 +0900

    値がNullではない場合true, Nullの場合falseを返却するｺﾝﾊﾞｰﾀ

---
+++ Mvvm/Converters/Null2BooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfUtilV2.Mvvm.Converters
+{
+    public class Null2BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is null);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
