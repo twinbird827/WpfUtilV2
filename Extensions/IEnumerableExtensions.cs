c19139bb345c781eeffcc576548324aa9d0f51e4
twinbird827 <twinbird827@gmail.com>
2019-05-21 15:43:46 +0900

    [add] 拡張処理の追加

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -8,6 +8,13 @@ namespace WpfUtilV2.Extensions
 {
     public static class IEnumerableExtensions
     {
+        /// <summary>
+        /// 指定した配列を<code>chunkSize</code>区切りのIEnumerable&lt;IEnumerable&lt;T&gt;&gt;として取得します。
+        /// </summary>
+        /// <typeparam name="T">配列内のｲﾝｽﾀﾝｽ型</typeparam>
+        /// <param name="source">元配列</param>
+        /// <param name="chunkSize">元配列を分割する区切り数</param>
+        /// <returns></returns>
         public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
         {
             if (chunkSize <= 0)
@@ -20,13 +27,52 @@ namespace WpfUtilV2.Extensions
             }
         }
 
+        /// <summary>
+        /// 最大値、またはﾃﾞﾌｫﾙﾄ値を取得します。
+        /// </summary>
+        /// <typeparam name="T">配列の型</typeparam>
+        /// <typeparam name="TResult">返却する値の型</typeparam>
+        /// <param name="source">配列</param>
+        /// <param name="func">返却する値を取得するﾛｼﾞｯｸ</param>
+        /// <returns></returns>
         public static TResult MaxOrDefault<T, TResult>(this IEnumerable<T> source, Func<T, TResult> func)
         {
             return source.MaxOrDefault(func, default(TResult));
         }
+
+        /// <summary>
+        /// 最大値、またはﾃﾞﾌｫﾙﾄ値を取得します。
+        /// </summary>
+        /// <typeparam name="T">配列の型</typeparam>
+        /// <typeparam name="TResult">返却する値の型</typeparam>
+        /// <param name="source">配列</param>
+        /// <param name="func">返却する値を取得するﾛｼﾞｯｸ</param>
+        /// <param name="def">ﾃﾞﾌｫﾙﾄ値</param>
+        /// <returns></returns>
         public static TResult MaxOrDefault<T, TResult>(this IEnumerable<T> source, Func<T, TResult> func, TResult def)
         {
             return source.Any() ? source.Max(func) : def;
+        }
+
+        /// <summary>
+        /// 指定した値の配列ｲﾝﾃﾞｯｸｽを取得します。
+        /// </summary>
+        /// <typeparam name="T">配列の型</typeparam>
+        /// <param name="source">配列</param>
+        /// <param name="item">検索値</param>
+        /// <returns></returns>
+        public static int IndexOf<T>(this IEnumerable<T> source, T item)
+        {
+            var i = 0;
+            foreach (var value in source)
+            {
+                if (object.Equals(value, item))
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
         }
     }
 }
