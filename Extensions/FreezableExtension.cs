c902b16613be2ed695b50f228b49dc2e58246aa4
twinbird827 <twinbird827@gmail.com>
2019-05-21 15:39:33 +0900

    [add] 拡張処理の追加

---
+++ Extensions/FreezableExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfUtilV2.Extensions
+{
+    public static class FreezableExtension
+    {
+        /// <summary>
+        /// FreezableをFrozenした値を取得します。
+        /// </summary>
+        /// <typeparam name="T">返却する際のFreezable</typeparam>
+        /// <param name="target">Freezableｲﾝｽﾀﾝｽ</param>
+        /// <returns></returns>
+        public static T Frozen<T>(this T target) where T : Freezable
+        {
+            return target.GetAsFrozen() as T;
+        }
+    }
+}
