968db15e723dac7a33189846ef93ca665a1a9c1e
twinbird827 <twinbird827@gmail.com>
2019-05-21 15:37:13 +0900

    [add] 拡張処理追加

--- Extensions/DoubleExtensions.cs
+++ Extensions/DoubleExtensions.cs
@@ -18,5 +18,21 @@ namespace WpfUtilV2.Extensions
         {
             return ((long)d).ToHex(length);
         }
+
+        /// <summary>
+        /// 指定した範囲内の値を取得します。
+        /// </summary>
+        /// <param name="target">対象値</param>
+        /// <param name="minimum">最小値</param>
+        /// <param name="maximum">最大値</param>
+        /// <returns>対象値が最小値、または最大値の範囲外なら最小値、または最大値を返却する。それ以外は対象値を返却する。</returns>
+        public static double InRange(this double target, double minimum, double maximum)
+        {
+            return target < minimum
+                ? minimum
+                : maximum < target
+                ? maximum
+                : target;
+        }
     }
 }
