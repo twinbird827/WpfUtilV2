e590e4609e358110d040f92084fc118f258ecd01
twinbird827 <twinbird827@gmail.com>
2019-05-31 14:56:45 +0900

    [modify] 表示専用のｺﾝﾊﾞｰﾀに変更

--- Mvvm/Converters/Double2StringFormatConverter.cs
+++ Mvvm/Converters/Double2StringFormatConverter.cs
@@ -10,76 +10,41 @@ namespace WpfUtilV2.Mvvm.Converters
 {
     public class Double2StringFormatConverter : IMultiValueConverter
     {
-        private Dictionary<string, FormattedData> Processeds = new Dictionary<string, FormattedData>();
-
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            FormattedData data;
-
             // 変換用ﾊﾟﾗﾒｰﾀの確認
             if (values == null)
             {
                 throw new ArgumentException("values is null.");
             }
-            else if (values.Length < 2)
+            else if (values.Length != 2)
             {
                 throw new ArgumentException("the number of values is less than 2.");
-            }
-            else if (parameter == null)
-            {
-                throw new ArgumentException("parameter is null.");
             }
             else if (values[0] == null)
             {
                 throw new ArgumentException("values[0] is null.");
             }
-
-            // 書式指定前の値を取得
-            if (values[0] is double)
+            else if (values[1] == null)
             {
-                data.Value = (double)values[0];
+                throw new ArgumentException("values[1] is null.");
             }
-            else if (!double.TryParse(values[0].ToString(), out data.Value))
+            else if (!(values[0] is double))
             {
                 throw new ArgumentException("values[0] is not double.");
             }
-
-            // 書式を取得
-            data.Format = (string)values[1];
-
-            // 変換ﾃﾞｰﾀとしてﾏｯﾋﾟﾝｸﾞする。
-            Processeds[parameter.ToString()] = data;
+            else if (!(values[1] is string))
+            {
+                throw new ArgumentException("values[1] is not string.");
+            }
 
             // ﾌｫｰﾏｯﾄした文字を返却
-            return string.Format(data.Format, data.Value);
+            return string.Format((string)values[1], values[0]);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            var previous = Processeds[parameter.ToString()];
-
-            double tmp;
-            if (value is string && double.TryParse((string)value, out tmp))
-            {
-                return new object[] { tmp, previous.Format };
-            }
-            else
-            {
-                return new object[] { previous.Value, previous.Format };
-            }
-        }
-
-        private struct FormattedData
-        {
-            public FormattedData(double value, string format)
-            {
-                this.Value = value;
-                this.Format = format;
-            }
-
-            public double Value;
-
-            public string Format;
+            throw new NotImplementedException();
         }
     }
 }
