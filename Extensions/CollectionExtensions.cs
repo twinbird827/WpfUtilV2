3260d8d274a5026da7d9c77c0c9309f15683adc1
twinbird827 <twinbird827@gmail.com>
2019-09-30 10:26:35 +0900

    [add] ｺﾚｸｼｮﾝの一括追加ﾒｿｯﾄﾞ

---
+++ Extensions/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtilV2.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> array)
+        {
+            foreach (var tmp in array)
+            {
+                collection.Add(tmp);
+            }
+        }
+    }
+}
