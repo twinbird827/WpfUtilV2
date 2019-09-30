8ac4a25f248ea3b6c19cbfb6f93e7b4659c583d9
twinbird827 <twinbird827@gmail.com>
2019-09-30 10:27:06 +0900

    [add] 指定した日の00:00:00と、23:59:59を取得するﾒｿｯﾄﾞを追加

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtilV2.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToBeginDays(this DateTime date)
+        {
+            return date
+                .AddHours(date.Hour * -1)
+                .AddMinutes(date.Minute * -1)
+                .AddSeconds(date.Second * -1)
+                .AddMilliseconds(date.Millisecond * -1);
+        }
+
+        public static DateTime ToEndDays(this DateTime date)
+        {
+            return date
+                .AddHours(23 - date.Hour)
+                .AddMinutes(59 - date.Minute)
+                .AddSeconds(59 - date.Second)
+                .AddMilliseconds(999 - date.Millisecond);
+        }
+    }
+}
