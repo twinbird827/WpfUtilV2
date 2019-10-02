7ed7a2eabab5bed6802f6cc959e89d17311a1d31
twinbird827 <twinbird827@gmail.com>
2019-10-02 09:33:02 +0900

    [add] FrameworkElement上でｸﾘｯｸした際に選択状態を保存できるﾋﾞﾍｲﾋﾞｱを追加

---
+++ Mvvm/Behaviors/FrameworkElementChangeIsSelectedWithMouseClickBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfUtilV2.Mvvm.CustomControls;
+
+namespace WpfUtilV2.Mvvm.Behaviors
+{
+    public static class FrameworkElementChangeIsSelectedWithMouseClickBehavior
+    {
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.RegisterAttached("IsEnabled",
+                typeof(bool),
+                typeof(FrameworkElementChangeIsSelectedWithMouseClickBehavior),
+                new PropertyMetadata(OnSetIsEnabledCallback)
+        );
+
+        public static bool GetIsEnabled(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsEnabledProperty);
+        }
+
+        public static void SetIsEnabled(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsEnabledProperty, value);
+        }
+
+        private static void OnSetIsEnabledCallback(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+
+            // PreviewKeyDownｲﾍﾞﾝﾄ時、選択を解除するｷｰ入力をｷｬﾝｾﾙする。
+            BehaviorUtil.SetEventHandler(element,
+                fe => fe.MouseLeftButtonDown += FrameworkElement_MouseLeftButtonDown,
+                fe => fe.MouseLeftButtonDown -= FrameworkElement_MouseLeftButtonDown
+            );
+        }
+
+        private static void FrameworkElement_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+
+            if (element == null) return;
+
+            if (!GetIsEnabled(element)) return;
+
+            var selectable = element.DataContext as ISingleSelectableItem;
+
+            if (selectable == null) return;
+
+            selectable.SelectableItems.AsParallel().ForAll(s => s.IsSelected = false);
+            selectable.IsSelected = true;
+        }
+    }
+}
---
+++ Mvvm/CustomControls/ISingleSelectableItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtilV2.Mvvm.CustomControls
+{
+    interface ISingleSelectableItem : ISelectableItem
+    {
+        IEnumerable<ISelectableItem> SelectableItems { get; }
+    }
+}
