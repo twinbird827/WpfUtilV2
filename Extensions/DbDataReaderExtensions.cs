7a6687d22f46c3e8d8a20fe53532b138ab35216e
twinbird827 <twinbird827@gmail.com>
2019-11-11 09:45:41 +0900

    [add] DbDataReaderを全行回してListを作成する拡張ﾒｿｯﾄﾞを追加

---
+++ Extensions/DbDataReaderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUtilV2.Extensions
+{
+    public static class DbDataReaderExtensions
+    {
+        public static async Task<IEnumerable<T>> GetRows<T>(this DbDataReader reader, Func<DbDataReader, T> func)
+        {
+            var ret = new List<T>();
+            while (await reader.ReadAsync())
+            {
+                ret.Add(func(reader));
+            }
+            return ret;
+        }
+    }
+}
